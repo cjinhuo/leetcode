91528826a0be3dcb496636e5b694da859c97a410
cjinhuo <1443970739@qq.com>
2020-08-11 11:15:18 +0800

    feat：添加饿了么的tree的优化

---
+++ tree/elementTree.js
@@ -0,0 +1,60 @@
+// 时间复杂度在O(n)~O(2n)之间
+function filter(value) {
+  const filterNodeMethod = this.filterNodeMethod;
+  const lazy = this.lazy;
+  const traverse = function (node) {
+    const childNodes = node.root ? node.root.childNodes : node.childNodes;
+
+    childNodes.forEach(child => {
+      child.visible = filterNodeMethod.call(child, value, child.data, child);
+
+      traverse(child);
+    });
+
+    if (!node.visible && childNodes.length) {
+      let allHidden = true;
+      // 需要优化的点
+      allHidden = !childNodes.some(child => child.visible);
+
+      if (node.root) {
+        node.root.visible = allHidden === false;
+      } else {
+        node.visible = allHidden === false;
+      }
+    }
+    if (!value) return;
+
+    if (node.visible && !node.isLeaf && !lazy) node.expand();
+  };
+
+  traverse(this);
+}
+
+// 时间复杂度稳定在O(n)
+function myFilter(value) {
+  const filterNodeMethod = this.filterNodeMethod;
+  const lazy = this.lazy;
+  const traverse = function (node) {
+    const childNodes = node.root ? node.root.childNodes : node.childNodes;
+    let allHidden = true;
+    childNodes.forEach(child => {
+      child.visible = traverse(child);
+      if (filterNodeMethod.call(child, value, child.data, child)) {
+        allHidden = false;
+        child.visible = true;
+      } else {
+        child.visible ? (allHidden = false) : (child.value = false);
+      }
+    });
+    if (!node.visible && childNodes.length) {
+      if (node.root) {
+        node.root.visible = allHidden === false;
+      }
+    }
+    if (!value) return true;
+    if (node.visible && !node.isLeaf && !lazy) node.expand();
+    // 用子元素返回给父元素
+    return !allHidden;
+  };
+  traverse(this);
+}
--- tree/treeFilter.js
+++ tree/treeFilter.js
@@ -1,34 +1,3 @@
-// function filterTree(nodes, keyword) {
-//   // 利用回溯，优先遍历子节点，遍历完子节点然后给父节点打上标签，时间复杂度为O(2n)
-//   const setFlag = (nodes, keyword) => {
-//     // 给父节点返回的标志
-//     let isParentNeed = false
-//     nodes.forEach(item => {
-//       if (item.children && item.children.length !== 0) {
-//         item.isNeed = setFlag(item.children, keyword)
-//       }
-//       if (item.txt.includes(keyword)) {
-//         isParentNeed ? '' : isParentNeed = true
-//         item.isNeed = true
-//       } else {
-//         item.isNeed ? isParentNeed = true : item.isNeed = false
-//       }
-//     })
-//     return isParentNeed
-//   }
-//   const filterNotIsNeed = nodes => {
-//     return nodes.filter(item => {
-//       if (item.children && item.children.length) {
-//         item.children = filterNotIsNeed(item.children)
-//       }
-//       return item.isNeed
-//     })
-//   }
-//   setFlag(nodes, keyword)
-//   return filterNotIsNeed(nodes)
-// }
-// console.log(filterTree(data, '测试'))
-
 const data = [
   {
     id: 1,
@@ -82,34 +51,6 @@ const data = [
   },
 ]
 
-// function filterTree(nodes, keyword) {
-//   // 利用回溯，优先遍历子节点，遍历完子节点然后给父节点打上标签，时间复杂度为O(n)
-//   const setFlagAndSplice = (nodes, keyword) => {
-//     // 给父节点返回的标志
-//     let isParentNeed = false
-
-//     nodes.forEach((item, index) => {
-//       if (item.children && item.children.length !== 0) {
-//         item.isNeed = setFlagAndSplice(item.children, keyword)
-//       }
-//       console.log(item.txt)
-//       if (item.txt.includes(keyword)) {
-//         isParentNeed ? '' : (isParentNeed = true)
-//         item.isNeed = true
-//       } else {
-//         item.isNeed ? (isParentNeed = true) : (item.isNeed = false)
-//       }
-//       if (!item.isNeed) {
-//         nodes.splice(index, 1)
-//       }
-//     })
-//     return isParentNeed
-//   }
-//   setFlagAndSplice(nodes, keyword)
-//   return nodes
-// }
-// console.log(filterTree(data, '测试121'))
-
 function filterTreeOne(nodes, keyword) {
   const setFlagAndRemoveDeepChild = curNodes => {
     let isParentNeed = false
@@ -135,7 +76,7 @@ function filterTreeOne(nodes, keyword) {
   return nodes
 }
 
-console.log(JSON.stringify(filterTreeOne(data, '测试121')))
+console.log(filterTreeOne(data, '测试121'))
 
 // let a = [1, 2, 3]
 // for (let i = 0; i < a.length; i++) {
@@ -167,3 +108,30 @@ console.log(JSON.stringify(filterTreeOne(data, '测试121')))
 //   }
 // }
 // recurseFor(data)
+// function filterTree(nodes, keyword) {
+//   // 利用回溯，优先遍历子节点，遍历完子节点然后给父节点打上标签，时间复杂度为O(n)
+//   const setFlagAndSplice = (nodes, keyword) => {
+//     // 给父节点返回的标志
+//     let isParentNeed = false
+
+//     nodes.forEach((item, index) => {
+//       if (item.children && item.children.length !== 0) {
+//         item.isNeed = setFlagAndSplice(item.children, keyword)
+//       }
+//       console.log(item.txt)
+//       if (item.txt.includes(keyword)) {
+//         isParentNeed ? '' : (isParentNeed = true)
+//         item.isNeed = true
+//       } else {
+//         item.isNeed ? (isParentNeed = true) : (item.isNeed = false)
+//       }
+//       if (!item.isNeed) {
+//         nodes.splice(index, 1)
+//       }
+//     })
+//     return isParentNeed
+//   }
+//   setFlagAndSplice(nodes, keyword)
+//   return nodes
+// }
+// console.log(filterTree(data, '测试121'))
