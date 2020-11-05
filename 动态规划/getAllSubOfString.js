0ce212d22750a0da35011295bf37de4c83035abd
jinhuochen <1443970739@qq.com>
2020-11-05 10:37:08 +0800

    feat：添加mergeSortedArray

--- 动态规划/getAllSubOfString.js
+++ 动态规划/getAllSubOfString.js
@@ -1,28 +1,29 @@
-// 字符串的子序列
-function getSubsequence(str, index = 0, result = '') {
-  if (index === str.length){
-    console.log(result)
-    return
-  }
-  getSubsequence(str, index + 1, result)
-  getSubsequence(str, index + 1, result + str[index])
+// 字符串的子序列 c b bc a ac ab abc
+function getSubsequence(str) {
+  const recursion = (target, index = 0, result = '') => {
+    if (index === target.length) {
+      console.log(result);
+      return;
+    }
+    recursion(target, index + 1, result);
+    recursion(target, index + 1, result + target[index]);
+  };
+  recursion(str);
 }
 
-// getSubsequenceOfString('abc')
+getSubsequence('abc');
 
-// 字符串的子串
-function getSubString(str){
-  const arr = []
+// 字符串的子串 [ 'a', 'ab', 'abc', 'b', 'bc', 'c' ]
+function getSubString(str) {
+  const arr = [];
   for (let i = 0; i < str.length; i++) {
-    let result = ''
+    let result = '';
     for (let j = i; j < str.length; j++) {
-      result = result + str[j]
-      arr.push(result)
+      result = result + str[j];
+      arr.push(result);
     }
   }
-  console.log(arr)
-  return arr
+  console.log(arr);
+  return arr;
 }
-getSubString('abc')
-
-
+// getSubString('abc');
