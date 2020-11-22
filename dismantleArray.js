28b4fcc685ea47d3b58475b1bfdbef7de5df8ec5
jinhuochen <1443970739@qq.com>
2020-11-22 17:30:22 +0800

    styled

--- dismantleArray.js
+++ dismantleArray.js
@@ -37,65 +37,18 @@ function dismantleArray(a, b) {
   return recurse(a, b);
 }
 const res = dismantleArray([1, [2, 3, [4]], 5, [7, 8, [10]], [null, 'o']], '[a, [b,z, g, y, [c], e], d, f,[t], [k,p,[o,[u]]]]');
-console.log(res); // res: { a: 1, b: 2, c: 4, e: undefined, d: 5 }
-
-// 王枫
-
-// const dismantleArray = (arr, str) => {
-//   const _arr = JSON.parse(str.replace(/([a-z])/g, '"$1"'));
-//   const flattenToData = (list, array, obj = {}) => {
-//     let save = 0; // 补偿系数
-//     list.forEach((item, key) => {
-//       if (typeof item === 'string') {
-//         obj[item] = typeof array[key + save] !== 'object' ? array[key + save] : undefined;
-//         return;
-//       }
-//       if (typeof item === 'object' && typeof array[key] !== 'object') {
-//         save += 1;
-//       }
-//       flattenToData(item, array[key + save], obj);
-//     });
-//     return obj;
-//   };
-//   return flattenToData(_arr, arr);
-// };
-// function dismantleArray(a, b) {
-//   try {
-//     b = JSON.parse(b.replace(/(\w)/g, '"$1"'));
-//   } catch (e) {
-//     console.log('输入字符串有误');
-//   }
-
-//   function parse(initArr, matchArr, initRet = {}) {
-//     for (let i = 0, j = 0; i < matchArr.length; i++, j++) {
-//       let key = matchArr[i];
-//       if (Array.isArray(key) && Array.isArray(initArr[j])) {
-//         parse(initArr[j], matchArr[i], initRet);
-//       } else if (!Array.isArray(key) && Array.isArray(initArr[j])) {
-//         if (!initRet[key]) {
-//           initRet[key] = undefined;
-//         }
-//         j = j - 1;
-//       } else if (Array.isArray(key) && !Array.isArray(initArr[j])) {
-//         j = j + 1;
-//         if (j < initArr.length) {
-//           while (Array.isArray(initArr[j])) {
-//             parse(initArr[j], matchArr[i], initRet);
-//             j = j + 1;
-//           }
-//         } else {
-//           parse([], matchArr[i], initRet);
-//         }
-//       } else {
-//         if (!initRet[key]) {
-//           initRet[key] = initArr[j];
-//         }
-//       }
-//     }
-//     return initRet;
-//   }
-
-//   return parse(a, b);
+console.log(res);
+// {
+//   a: 1,
+//   b: 2,
+//   z: 3,
+//   g: undefined,
+//   y: undefined,
+//   c: 4,
+//   e: undefined,
+//   d: 5,
+//   f: undefined,
+//   t: 7,
+//   k: null,
+//   p: 'o'
 // }
-// const res = dismantleArray([1, [2, 3, [4]], 5, [7, 8, [10]], [null, 'o']], '[a, [b,z, g, y, [c], e], d, f,[t], [k,p,[o,[u]]]]');
-// console.log(res); // { a: 1, b: 2, c: 4, e: undefined, d: 5, f:undefined }
