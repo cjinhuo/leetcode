e0356efaa2474fa7ab5e85f21d7577e7179e5799
jinhuochen <1443970739@qq.com>
2021-06-30 19:19:27 +0800

    feat(InOrder): iterationInOrder

--- tree/InOrder.js
+++ tree/InOrder.js
@@ -1,12 +1,26 @@
 let tree = require('./treeData')
-// 中序遍历 左中右 
+// 中序遍历 左中右
 function inOrder(root, result = []) {
   if (root === null) return
   inOrder(root.left, result)
   result.push(root.val)
   inOrder(root.right, result)
-  console.log(result)
   return result
 }
+console.log(inOrder(tree))
 
-inOrder(tree)
+function iterationInOrder(root) {
+  const stack = []
+  const result = []
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    result.push(root.val)
+    root = root.right
+  }
+  return result
+}
+iterationInOrder(tree)
