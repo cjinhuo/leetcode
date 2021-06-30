cbd428b6267ebb66be14e4f821533c587fb6375f
jinhuochen <1443970739@qq.com>
2021-06-30 22:31:26 +0800

    feat:fix getMax

--- company-written-test/ali/one.js
+++ company-written-test/ali/one.js
@@ -4,13 +4,20 @@ function getMax(arr) {
   for (let i = length - 1; i >= 0; i--) {
     const nextIndex = i + 2
     if (nextIndex > length - 1) continue
-    arr[i] = arr[i] + arr[nextIndex]
-    console.log(arr[i])
+    const last = nextIndex + 1
+    if (last <= length - 1) {
+      arr[i] = arr[i] + arr[last] > arr[i] + arr[nextIndex] ? arr[i] + arr[last] : arr[i] + arr[nextIndex]
+    } else {
+      arr[i] = arr[i] + arr[nextIndex]
+    }
     if (arr[i] > max) {
       max = arr[i]
     }
   }
+  console.log(arr, max)
   return max
 }
 
-getMax([2, 0, 0, 4, 5])
+getMax([2, 0, 0, 5, 4, 6, 8])
+// [2, 0, 0, 5, 4, 6, 8]
+// [15, 15, 13, 13, 12, 6, 8]
